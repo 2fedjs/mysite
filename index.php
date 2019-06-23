abcb39aa3fbe526bda7d08b85e5d7b6b479f1cd8
2fedjs <49389822+2fedjs@users.noreply.github.com>
2019-06-23 18:00:52 +0500

    ыва
    аыв

--- index.php
+++ index.php
@@ -24,7 +24,130 @@
 	</style>
 </head>
 <body>
-	<form action="">
+	<select name="" id="">
+		<?php
+			for($i = 1910; $i <= 2010; $i++){
+				echo "<option value=\"$i\">$i</option>";
+			}
+
+		?>
+
+	</select>
+	<?php
+			$h = 23;
+			$a = 50;
+			$str1 = '$a приветов!';
+			$str2 = "$a приветов!";
+			echo $str1;
+			echo "<br>";
+			echo $str2;
+			echo "<br>";
+			echo $h/6-($h%6)/6;
+			echo "<br>";
+
+			function area($r){
+				return 3.14*$r*$r;
+			}
+			echo area(5) - area(3);
+			echo "<br>";
+
+			$minuts = 11;
+			$ost = $minuts%10;
+
+			function check_time($minuts){
+				$ost = $minuts%10;
+				if($minuts>10 && $minuts<15){
+				echo "$minuts минут";
+				echo "<br>";
+				}
+				elseif ($ost==1){
+					echo "$minuts минута";
+					echo "<br>";
+				}
+				elseif($ost>=2 && $ost<5){
+					echo "$minuts минуты";
+					echo "<br>";
+				}
+				else{
+					echo "$minuts минут";
+					echo "<br>";
+				}
+			}
+			
+			/*for($i=1;$i<60;$i++){
+				check_time($i);
+			}*/
+
+			$arr = [
+				 'Armenia1' => 'Armenia', 
+			    'Georgia' => 'Georgia', 
+			    'Albania' => 'Albania', 
+			    'Algeria' => 'Algeria', 
+			    'Bahrain' => 'Bahrain', 
+			    'Barbados' => 'Barbados'
+			];
+
+			/*foreach($arr as $country =>$capital){
+				echo "$country<br>";
+			}*/
+
+			$capitals = [
+				'Россия1' => ['Москва1','Москва2','Москва3','Москва4','Москва5'],
+				'Россия2' => ['Москва1','Москва2','Москва3','Москва4'],
+				'Россия3' => ['Москва1','Москва2','Москва3'],
+				'Россия4' => ['Москва1','Москва2'],
+				'Россия5' => ['Москва1']
+			];
+
+			/*echo '<ul>';
+			foreach($capitals as $capital =>$towns){
+				echo "<li>$capital<ul>";
+					foreach($towns as $town){
+						echo "<li>$town</li>";
+					}
+				echo "</ul></li>";
+			}
+			echo '</ul>';*/
+			$id = $_GET['id'];
+			$text = file_get_contents("data/$id");
+			echo nl2br($text);
+			echo "<br>";
+
+			$files = scandir('data');
+			foreach($files as $file){
+				if(is_file('data/'.$file)){
+					echo "<a href=\"index.php/?id=$file\">$file</a>";
+				}
+			}
+			echo "<br>";
+			if(count($_POST>0)){
+				$name = $_POST['name'];
+				$phone = $_POST['phone'];
+				file_put_contents('apps.txt', "$name $phone\n", FILE_APPEND);
+				echo 'ваша заявка принята';
+			}
+
+		?>
+		<ul>
+			<?php foreach($capitals as $capital => $towns): ?>
+				<li><?=$capital?>
+					<ol>
+						<?php foreach($towns as $town): ?>
+							<li><?=$town?></li>
+						<?php endforeach; ?>
+					</ol>
+				</li>
+			<?php endforeach; ?>
+		</ul>
+	<form id="backcall" action="" method="post">
+		Имя<br>
+		<input type="text" name="name"><br>
+		Телефон<br>
+		<input type="text" name="phone"><br>
+		<input type="submit" value="Отправить">
+	</form>
+	<hr>
+	<form id="reg" action="">
 		Имя<br><input type="text" name="name"><span class="error"></span><br>
 		Телефон<br><input type="text" name="phone"><span class="error"></span><br>
 		Почта<br><input type="text" name="email"><span class="error"></span><br>
@@ -33,12 +156,12 @@
 	<div class="result"></div>
 	<script>
 		$(function(){
-			let $form = $("form");
+			let $form = $("#reg");
 			let $res = $(".result");
-			let $but = $("form>input[type=button]");
-			let $inputs = $("input[type=text]");
+			let $but = $("#reg>input[type=button]");
+			let $inputs = $("#reg>input[type=text]");
 
-			$("form>input[type=button]").on("click", function(){
+			$("#reg>input[type=button]").on("click", function(){
 				$but.attr("disabled", true);
 				$res.addClass("loading");
 				$.post("test/ajaxtest1.php",$form.serialize(), function(data){
@@ -50,10 +173,10 @@
 						$form[0].reset();
 					} else {
 						for(let key in data.errors){
-							$("input[name="+key+"]" ).next().html(data.errors[key]);
+							$("#reg>input[name="+key+"]" ).next().html(data.errors[key]);
 						}
 					}
-				});
+				},"json");
 			});
 		});
 	</script>
